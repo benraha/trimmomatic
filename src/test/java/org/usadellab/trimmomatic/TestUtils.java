71b2ab47a3a5a6ea34a2112ccaffe05b671f6f96
Ben Rahamim <benraha@gmail.com>
2019-07-21 10:59:30 +0300

    Fixed deprecated method

--- src/test/java/org/usadellab/trimmomatic/TestUtils.java
+++ src/test/java/org/usadellab/trimmomatic/TestUtils.java
@@ -4,13 +4,14 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestUtils {
 
     static void assertTwoFiles(File expected, File actual) throws IOException {
-        assertEquals(FileUtils.readFileToString(expected), FileUtils.readFileToString(actual));
+        assertEquals(FileUtils.readFileToString(expected, Charset.defaultCharset()), FileUtils.readFileToString(actual, Charset.defaultCharset()));
 
     }
 
